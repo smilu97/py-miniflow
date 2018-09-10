e6962694b71199e8f67f208b01dc228efd115805
smilu97 <smilup2244@gmail.com>
2018-09-10 22:04:03 +0900

    add xavier

--- flow/__init__.py
+++ flow/__init__.py
@@ -3,6 +3,12 @@ from flow.session import Session
 from flow.node.base import *
 import progressbar as pb
 
+def xavier(*shape, uniform=False):
+    fan_in = shape[0]
+    fan_out = shape[-1]
+    x = np.sqrt((6 if uniform else 2) / (fan_in + fan_out))
+    return (np.random.rand if uniform else np.random.randn)(*shape) * x
+
 def xor_test():
 
     sess = Session(lr=0.1)
@@ -19,13 +25,13 @@ def xor_test():
     
     y = PlaceholderNode(sess, train_y, 'y')
 
-    V0 = VariableNode(sess, np.random.rand(2,2))
-    b0 = VariableNode(sess, np.random.rand(2))
+    V0 = VariableNode(sess, xavier(2,2))
+    b0 = VariableNode(sess, xavier(2))
     b0_tiled = TileNode(sess, [b0], 4, 0)
     S0 = SigmoidNode(sess, [AddNode(sess, [MatmulNode(sess, [x, V0]), b0_tiled])])
 
-    V1 = VariableNode(sess, np.random.rand(2,1))
-    b1 = VariableNode(sess, np.random.rand(1))
+    V1 = VariableNode(sess, xavier(2,1))
+    b1 = VariableNode(sess, xavier(1))
     b1_tiled = TileNode(sess, [b1], 4, 0)
     S1 = SigmoidNode(sess, [AddNode(sess, [MatmulNode(sess, [S0, V1]), b1_tiled])])
 
